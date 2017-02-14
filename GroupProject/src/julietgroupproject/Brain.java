1c7cf543e2ce7baedef5d19456f013352f268413
Sizhe Sun <ss2324@cam.ac.uk>
2017-02-14 08:23:04 +0000

    Fixed ANN input normalisation, changed behaviour when v < MIN_VELOCITY.

--- GroupProject/src/julietgroupproject/Brain.java
+++ GroupProject/src/julietgroupproject/Brain.java
@@ -13,12 +13,20 @@ import com.jme3.scene.control.AbstractControl;
 import org.encog.ml.MLRegression;
 import org.encog.ml.data.MLData;
 import org.encog.ml.data.basic.BasicMLData;
-import org.encog.neural.networks.BasicNetwork;
 
 public class Brain extends AbstractControl {
-    // Brain is created when the Alien class is instatiated in the simulation.
-    //It has a list of the limbs (Geometries), joints (HingeJoints) and the graphical node all the geometries are attached to.
-    // These can be used later e.g. by the neural network
+    
+    /*
+     * Brain is created when the Alien class is instatiated in the simulation.
+     * It is added to the rootNode of Alien as a Control,
+     * which will control the alien using a provided ANN,
+     * set by the setNN() method.
+     * 
+     * TODO: adapt to refactored Simulator class and
+     * better encapsulation.
+     * 
+     * @author ss2324
+     */
 
     public ArrayList<HingeJoint> joints = new ArrayList<HingeJoint>();
     public ArrayList<Geometry> geometries = new ArrayList<Geometry>();
@@ -28,30 +36,33 @@ public class Brain extends AbstractControl {
     private static final float MAX_VELOCITY = 2f;
     private static final float MIN_VELOCITY = 0.1f;
     private static final float MAX_POWER = 1f;
-    private static final int TICK_CYCLE = 100;
+    private static final int TICK_CYCLE = 50;
     private int tick = 0;
 
     public Vector3f getPosition(Node node) {
         return node.getControl(RigidBodyControl.class).getPhysicsLocation();
     }
 
-    public void still() {
-        for (Geometry g : geometries) {
-            g.getControl(RigidBodyControl.class).setAngularVelocity(Vector3f.ZERO);
-        }
-    }
-
     public void setNN(MLRegression nn) {
         this.nn = nn;
     }
-
+    
     private void updateInput() {
         /*
          * fetch physical information into nnInput.
          */
 
         for (int i = 0; i < nnInput.length; i++) {
-            nnInput[i] = (double) joints.get(i).getHingeAngle();
+            
+            // normalise input to range from 0 to 1
+            // Angles are in radians. 0 indicates no
+            // rotation, positive and negative values
+            // denote clockwise and anticlockwise rotations
+            // (PS: I'm not sure about which is which though)
+            double in = ((double)joints.get(i).getHingeAngle())/(1.5 * Math.PI) + 0.5;
+            if (in < 0.0) in = 0.0;
+            if (in > 1.0) in = 1.0;
+            nnInput[i] = in;
         }
     }
 
@@ -61,16 +72,21 @@ public class Brain extends AbstractControl {
 
         // initialise nn input/output arrays
         if (spatial != null) {
-            nnInput = new double[((BasicNetwork) (this.nn)).getInputCount()];
+            nnInput = new double[nn.getInputCount()];
         }
     }
 
     @Override
     protected void controlUpdate(float tpf) {
         /*
-         * 
+         * Updates state of alien.
+         * Every TICK_CYCLE ticks this method will
+         * pull new information from the simulation.
+         * Current implementation uses angles of joints.
+         * It will then feed the input to the ANN, computing
+         * output and reflect back to the simulation.
          */
-
+        
         if (nn == null) {
             System.err.println("No neural network set.");
             return;
@@ -93,8 +109,8 @@ public class Brain extends AbstractControl {
                 float v = MAX_VELOCITY * (float) (2 * (nnOutput[i] - 0.5));
                 float p = MAX_POWER;
                 if (Math.abs(v) < MIN_VELOCITY) {
-                    // stop moving
-                    j.enableMotor(false, 0f, 0f);
+                    // try to stop moving
+                    j.enableMotor(true, 0f, 0f);
                     // debug
                     System.out.println("Suspending limb #" + i);
                 } else {
