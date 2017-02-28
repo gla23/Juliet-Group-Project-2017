ed2a118d64080564c8a9d4f704f152f8b4d2de7b
GeorgeProsser <georgeprosser2@gmail.com>
2017-02-28 11:33:08 +0000

    Added logger class

---
+++ GroupProject/src/Logger.java
@@ -0,0 +1,47 @@
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author George
+ */
+public class Logger {
+    
+    private int limit = 15;
+    private ArrayDeque<String> entries = new ArrayDeque<>();
+    
+    public Logger(int limit) {
+        this.limit = limit;
+    }
+    
+    public void push(String entry) {
+        entries.push(entry);
+        
+        while (entries.size() > limit) {
+            entries.removeLast();
+        }
+    }
+    
+    public int getSize() {
+        return entries.size();
+    }
+    
+    public int getLimit() {
+        return limit;
+    }
+    
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+    
+    public ArrayList<String> getEntries() {
+        return new ArrayList(entries);
+    }
+}
