e220e34e7dd6b2b94c57498ce3aec01930792e0b
George Andersen <gla23@cam.ac.uk>
2017-03-04 14:41:07 +0000

    Fixed limb seperation

--- GroupProject/src/julietgroupproject/UIAppState.java
+++ GroupProject/src/julietgroupproject/UIAppState.java
@@ -373,7 +373,7 @@ public class UIAppState extends DrawingAppState implements ActionListener {
         // currentHingeAxis Will be either "X", "Y", "Z" or "A" for auto
 
 
-        Vector3f newPos = contactPt.add(normal.mult(Math.max(Math.max(limbLength, limbHeight), limbWidth) + limbSeparation));
+        Vector3f newPos = contactPt.add(normal.mult(limbWidth + limbSeparation));
 
         //Build the new limb
         Matrix3f rotation = new Matrix3f();
@@ -744,11 +744,9 @@ public class UIAppState extends DrawingAppState implements ActionListener {
         //TODO fix this so that is gets the actual distance, and also make that distance correct when it is rotated
         Vector3f newHingePos;
         Vector3f newPos;
-        if (currentShape.equals("Cylinder")) {
-            newPos = contactPt.add(normal.mult(limbWidth + limbSeparation));
-        } else {
-            newPos = contactPt.add(normal.mult(Math.max(Math.max(limbLength, limbHeight), limbWidth) + limbSeparation));
-        }
+        newPos = contactPt.add(normal.mult(limbWidth + limbSeparation));
+        //newPos = contactPt.add(normal.mult(Math.max(Math.max(limbLength, limbHeight), limbWidth) + limbSeparation));
+        
         //Vector3f 
         newHingePos = contactPt.add(normal.mult(4 * jointPositionFraction));
         // Work out which hinge axis would make sense for auto hinge axis
